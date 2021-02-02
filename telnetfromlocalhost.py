f7cb44e6b5071dc40f45a06dfc94222de51b7a6f
Austinstevesk <austinstevesk@gmail.com>
2021-02-02 07:56:08 +0300

    Added a localhost as the host

---
+++ telnetfromlocalhost.py
@@ -0,0 +1,21 @@
+from telnetlib import Telnet
+import getpass
+
+host = "http://localhost:8000/"
+user = raw_input("Enter your remote account: ")
+password = getpass.getpass()
+
+
+tn = Telnet(host)
+tn.read_until("login")
+tn.read_until(b'password')
+tn.write(password.encoded('ascii')+b'\n')
+
+tn.read_until(b'<Huawei>').decode('ascii')
+
+
+tn.close()
+
+tn.write(b'System view')
+
+tn.write(b'VLAN 10')
